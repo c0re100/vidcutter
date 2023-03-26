89f4c44bad2f40510ec4e005f30ed3002f8f7ab2
c0re100 <corehusky@gmail.com>
2023-03-27 04:08:36 +0800

    Lower maximum bitrate and crf 23

--- vidcutter/libs/videoservice.py
+++ vidcutter/libs/videoservice.py
@@ -226,10 +226,10 @@ class VideoService(QObject):
 
     def cut(self, source: str, output: str, frametime: str, duration: str, allstreams: bool = True, gifOutput: bool = False, mp4Output: bool = False, webmOutput: bool = False) -> bool:
         self.checkDiskSpace(output)
-        args = '-y -ss {0} -i "{1}" -t {2} -c:v libx264 -an -sn -map_chapters -1 -map_metadata -1 -crf 22 -pix_fmt yuv420p ' + \
-            '-vf "scale=iw*min(1\,min(1280/iw\,720/ih)):-2" -maxrate 10M -bufsize 10M -preset slow "{3}"'
-        webmArgs = '-y -ss {0} -i "{1}" -t {2} -c:v libvpx-vp9 -an -sn -map_chapters -1 -map_metadata -1 -crf 22 -pix_fmt yuva420p ' + \
-            '-vf "scale=iw*min(1\,min(512/iw\,512/ih)):-2" -maxrate 10M -bufsize 10M -preset slow "{3}"'
+        args = '-y -ss {0} -i "{1}" -t {2} -c:v libx264 -an -sn -map_chapters -1 -map_metadata -1 -crf 23 -pix_fmt yuv420p ' + \
+            '-vf "scale=iw*min(1\,min(1280/iw\,720/ih)):-2" -maxrate 8M -bufsize 8M -preset slow "{3}"'
+        webmArgs = '-y -ss {0} -i "{1}" -t {2} -c:v libvpx-vp9 -an -sn -map_chapters -1 -map_metadata -1 -crf 23 -pix_fmt yuva420p ' + \
+            '-vf "scale=iw*min(1\,min(512/iw\,512/ih)):-2" -maxrate 1M -bufsize 1M -b:v 1M -preset slow "{3}"'
 
         timestamp = int(time.time())
         file_path = QDir.fromNativeSeparators(output)
