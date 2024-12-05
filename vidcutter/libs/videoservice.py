38ba0df85594ad06ac7c74606f54a5c07bac1818
c0re100 <corehusky@gmail.com>
2024-12-06 02:52:39 +0800

    Set Animated WebP FPS to 23 and use Lanczos to downscaling

--- vidcutter/libs/videoservice.py
+++ vidcutter/libs/videoservice.py
@@ -229,7 +229,7 @@ class VideoService(QObject):
         args = '-y -ss {0} -i "{1}" -t {2} -c:v libx264 -an -sn -map_chapters -1 -map_metadata -1 -crf 22 -pix_fmt yuv420p ' + \
             '-vf "scale=iw*min(1\,min(1280/iw\,720/ih)):-2" -maxrate 8M -bufsize 8M -preset slow "{3}"'
         webpArgs = '-y -ss {0} -i "{1}" -t {2} -c:v libwebp_anim -compression_level 5 -q:v 80 -loop 0 -pix_fmt bgra ' + \
-            '-vf "scale=iw*min(1\,min(640/iw\,360/ih)):-2" "{3}"'
+            '-vf "scale=iw*min(1\,min(640/iw\,360/ih)):-2:flags=lanczos,fps=fps=23" "{3}"'
         webmArgs = '-y -ss {0} -i "{1}" -t {2} -c:v libvpx-vp9 -an -sn -map_chapters -1 -map_metadata -1 -crf 31 -pix_fmt yuva420p ' + \
             '-vf "scale=iw*min(1\,min(512/iw\,512/ih)):-2" -maxrate 256K -bufsize 256K -b:v 256K -preset slow "{3}"'
 
